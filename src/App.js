1ea99dd30259424613a43e5c2bd075e2e8a31f73
Nika-Firada <nika.firada@gmail.com>
2022-03-20 18:54:12 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import React from 'react';
+import "./App.css"
+import SearchIcon from '@mui/icons-material/Search';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './Header';
+import Country from './Country';
+import CountryDetail from './CountryDetail';
+import { useNavigate}  from 'react-router-dom';
+
+
+
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [countries, setCountrise] = useState([]);
+  const countriesInputRef = useRef();
+  const regionRef = useRef();
+  // const navigate = useNavigate();
+
+  const noCountries = countries.status || countries.message;
+  
+
+  const switchMode = () =>{
+    setDarkMode((prevState) => !prevState);
+  };
+
+  useEffect(()=>{
+    try {
+      fetchData()
+    } catch (error){
+      console.log(error)
+    }
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch('https://restcountries.com/v2/all');
+    const data = await response.json();
+    setCountrise(data);
+  }
+
+  const searchCountries = () =>{
+    const searchValue = countriesInputRef.current.value;
+    if(searchValue.trim()){
+      const fetchSearch = async () => {
+        const response = await fetch(`https://restcountries.com/v2/name/${searchValue}`)
+        const data = await response.json();
+        setCountrise(data);
+      }
+      try {
+        fetchSearch()
+      } catch (error){
+        console.log(error);
+      }
+    } else {
+      fetchData();
+    }
+  };
+
+  const selectRegion = () =>{
+    const regionValue = regionRef.current.value;
+    if(regionValue.trim()){
+      const fetchSelect = async () =>{
+        const response = await fetch(`https://restcountries.com/v2/region/${regionValue}`);
+        const data = await response.json();
+        
+        if(regionValue === 'All'){
+          try {
+            fetchData()
+          } catch (error) {
+            console.log(error);
+          }
+          return;
+        }
+        setCountrise(data);
+      };
+      try {
+        fetchSelect();
+      } catch (error){
+        console.log(error);
+      }
+    }
+  }
+  const showDetails = (code) =>{
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className={`app ${darkMode ? `darkMode` : ""}`}>
+        <Header onClick={switchMode} darkMode={darkMode} />
+        <Routes>
+          <Route path='/' element={
+            <div className="app_body">
+              <div className="inputs">
+                <div className={`search_input ${darkMode ? `darkMode` : ""}`}>
+                  <SearchIcon />
+                  <input type="text" placeholder='Search for a country...' ref={countriesInputRef} onChange={searchCountries}/>
+                </div>
+                <div className={`select_region ${darkMode ? `darkMode` : ""}`}>
+                  <select ref={regionRef} onChange={selectRegion}>
+                    <option>All</option>
+                    <option>Africa</option>
+                    <option>Americas</option>
+                    <option>Asia</option>
+                    <option>Europe</option>
+                    <option>Oceania</option>
+                  </select>
+                </div>
+              </div>
+              <div className="countries">
+                {
+                  !noCountries ? (
+                    countries.map((country )=>(
+                      <Country
+                        darkMode={darkMode}
+                        key={country.name} 
+                        code={country.apha3code}
+                        name={country.name}
+                        capital={country.capital}
+                        population={country.population}
+                        region={country.region}
+                        flag={country.flag}
+                        showDetails={showDetails}
+                      />
+                    ))
+                  ) : (
+                    <p>No Contries found...</p>
+                  )
+                }
+              </div>
+            </div>
+          } />
+          <Route path='country-details' element={<CountryDetail darkMode={darkMode} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/CountryDetail.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+function CountryDetail({darkMode}) {
+  return (
+    <div className='country_details'>
+        <button className={`back ${darkMode ? `darkMode` : ""}`}>
+            <ArrowBackIcon />
+            <p>Go Back</p>
+        </button>
+        <div className="country_details_body">
+            <div className='img_container'>
+                <img src="" alt="" />
+            </div>
+            <div className="info">
+                <h2>Name</h2>
+                <div className="info_container">
+                    <div className="left_info">
+                        <p>Native Name: <span className={`values ${darkMode ? `darkMode` : ""}`}>Test</span>    
+                        </p>
+                        <p>Population: <span className={`values ${darkMode ? `darkMode` : ""}`}>Test</span>    
+                        </p>
+                        <p>Region: <span className={`values ${darkMode ? `darkMode` : ""}`}>Test</span>    
+                        </p>
+                        <p>Sub region: <span className={`values ${darkMode ? `darkMode` : ""}`}>Test</span>    
+                        </p>
+                    </div>
+                    <div className="right_info">
+                        <p>Capital: <span className={`values ${darkMode ? `darkMode` : ""}`}>Test</span>    
+                        </p>
+                        <p>Top level domain: <span className={`values ${darkMode ? `darkMode` : ""}`}>Test</span>    
+                        </p>
+                        <p>Currencies: <span className={`values ${darkMode ? `darkMode` : ""}`}>Test</span>    
+                        </p>
+                        <p>Languages: <span className={`values ${darkMode ? `darkMode` : ""}`}>Test</span>    
+                        </p>
+                    </div>
+                </div>
+                Border Countries:
+                <div className={`border_country ${darkMode ? `darkMode` : ""}`}>
+                    <p>Test</p>
+                </div>
+                <div className={`border_country ${darkMode ? `darkMode` : ""}`}>
+                    <p>Test</p>
+                </div>
+                <div className={`border_country ${darkMode ? `darkMode` : ""}`}>
+                    <p>Test</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CountryDetail
